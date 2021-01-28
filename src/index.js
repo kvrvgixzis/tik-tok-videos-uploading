f16b479c1e238ee0ce26800fba17d11378b75385
Nikita Parfenov <n1koss.boy@gmail.com>
2021-01-28 19:52:35 +0500

    wip

--- src/index.js
+++ src/index.js
@@ -1,65 +1,45 @@
 const puppeteer = require('puppeteer');
-const http = require('http');
+const axios = require('axios');
+require('dotenv').config();
 
-const TIKTOK_URL = 'https://www.tiktok.com/upload';
-const MLA_PORT = 35000;
-const PROXY_URL =
-  'http://me-pvt-97.airsocks.in/api/v3/changer_channels/channel_1?session=nv1db5d5ehu5gyd7oxq72h2dudj3f7fn';
+const TIKTOK_URL = 'https://www.tiktok.com/upload/?lang=ru-RU';
+const { MLA_PORT, PROXY_URL, PROXY_USERNAME, PROXY_PASSWORD } = process.env;
 
 const startProfile = async () => {
   const profileId = '0ff2b104-351b-4a24-bce8-a50129fd7776';
   const mlaUrl = `http://127.0.0.1:${MLA_PORT}/api/v1/profile/start?automation=true&puppeteer=true&profileId=${profileId}`;
 
-  http
-    .get(mlaUrl, (resp) => {
-      let data = '';
-      // Receive response data by chunks
-      resp.on('data', (chunk) => {
-        data += chunk;
-      });
-      // The whole response data has been received.
-      // Handling JSON Parse errors, verifying if ws
-      // is an object and contains the 'value' parameter.
-      resp.on('end', () => {
-        let ws;
-        try {
-          ws = JSON.parse(data);
-        } catch (err) {
-          console.log(err);
-        }
-        if (typeof ws === 'object' && ws.hasOwnProperty('value')) {
-          console.log(`Browser ws endpoint: ${ws.value}`);
-          run(ws.value);
-        }
-      });
-    })
-    .on('error', (err) => {
-      console.log(err.message);
-    });
+  // starting multilogin profile
+  try {
+    console.log('>>> try start multilogin profile');
+    const response = await axios(mlaUrl);
+    console.log('>>> multilogin response: ', response.data);
+    if (response.data.status === 'OK') {
+      console.log(`>>> browser ws endpoint: ${response.data.value}`);
+      run(response.data.value);
+    }
+  } catch (error) {
+    console.log(error.response.body);
+  }
 };
 
 const reloadProxy = async () => {
-  http
-    .get(PROXY_URL, (resp) => {
-      let data = '';
-
-      resp.on('data', (chunk) => {
-        data += chunk;
-      });
-
-      resp.on('end', async () => {
-        // const answer = await data.text();
-        console.log('data: ', data);
-      });
-    })
-    .on('error', (err) => {
-      console.log(err.message);
-    });
+  try {
+    console.log('>>> try reload proxy');
+    const response = await axios(PROXY_URL);
+    console.log('>>> proxy response: ', response.data);
+  } catch (error) {
+    console.log(error.response.body);
+    await reloadProxy();
+  }
 };
 
 const run = async (ws) => {
+  // await while proxy reloaded
   await reloadProxy();
+
   try {
+    console.log('>>> start puppeteer');
     const browser = await puppeteer.connect({
       browserWSEndpoint: ws,
       defaultViewport: null,
@@ -67,12 +47,17 @@ const run = async (ws) => {
     });
 
     const [page] = await browser.pages();
+    // login to proxy
+    await page.authenticate({
+      username: PROXY_USERNAME,
+      password: PROXY_PASSWORD,
+    });
 
     await page.goto(TIKTOK_URL);
     console.log('page loaded');
     await browser.close();
   } catch (err) {
-    console.log('err: ', err);
+    console.log('puppeteer error: ', err);
   }
 };
 
